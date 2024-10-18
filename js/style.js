8f3691d23a97e35170a0421c7e97505615f1af1a
baptiste.freminet <baptiste.freminet@lycee-elie-vinet.org>
2024-10-18 16:37:49 +0200

    création caroussel html css js

--- js/style.js
+++ js/style.js
@@ -1,3 +1,17 @@
-// document.getElementsByClassName('.carousel').carousel({
-    // interval: 2000
-// })
+document.getElementById('icon-carousel-1').onclick=function(){
+    change_icon(document.getElementById('icon-carousel-1'))
+}
+document.getElementById('icon-carousel-2').onclick=function(){
+    change_icon(document.getElementById('icon-carousel-2'))
+}
+document.getElementById('icon-carousel-3').onclick=function(){
+    change_icon(document.getElementById('icon-carousel-3'))
+}
+
+
+function change_icon(active_icon){
+    document.getElementById('icon-carousel-1').classList.remove('active')
+    document.getElementById('icon-carousel-2').classList.remove('active')
+    document.getElementById('icon-carousel-3').classList.remove('active')
+    active_icon.classList.add('active');
+}
